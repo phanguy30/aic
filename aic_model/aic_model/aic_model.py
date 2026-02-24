1783f2ee9d071652eea7d24e887477f0ca8b3ef8
Morgan Quigley <morganquigley@intrinsic.ai>
2026-02-24 15:35:49 -0500

    remove dead code of old set_pose_target()

--- aic_model/aic_model/aic_model.py
+++ aic_model/aic_model/aic_model.py
@@ -191,7 +191,6 @@ class AicModel(LifecycleNode):
         if self._target_mode != TargetMode.MODE_CARTESIAN:
             self.get_logger().info("Setting cartesian mode...")
             self.set_target_mode(TargetMode.MODE_CARTESIAN)
-            self._target_mode = TargetMode.MODE_CARTESIAN
         self.motion_update_pub.publish(motion_update)
         return True
 
@@ -199,7 +198,6 @@ class AicModel(LifecycleNode):
         if self._target_mode != TargetMode.MODE_JOINT:
             self.get_logger().info("Setting joint mode...")
             self.set_target_mode(TargetMode.MODE_JOINT)
-            self._target_mode = TargetMode.MODE_JOINT
         self.joint_motion_update_pub.publish(joint_motion_update)
         return True
 
@@ -229,49 +227,6 @@ class AicModel(LifecycleNode):
                 "Either motion_update or joint_motion_update must be provided."
             )
             return False
-
-    def set_pose_target(self, pose: Pose, frame_id: str = "base_link"):
-        """Set a pose target for the robot arm.
-
-        The robot can be controlled in several different ways. This function
-        is intended to be the simplest way to move the arm around, by sending
-        a desired pose (position and orientation) for the gripper's
-        "tool control point" (TCP), which is the "pinch point" between the very
-        end of the gripper fingers. The rest of the control stack will take care
-        of moving all the arm's joints to so that the gripper TCP ends up in
-        the desired position and orientation.
-
-        The constants defined in this function are intended to provide
-        reasonable default behavior if the arm is unable to achieve the
-        requested pose. Different values for stiffness, damping, wrenches, and
-        so on can be used for different types of arm behavior. These values
-        are only intended to provide a starting point, and can be adjusted as
-        desired.
-        """
-
-        motion_update_msg = MotionUpdate()
-        motion_update_msg.pose = pose
-        motion_update_msg.header.frame_id = frame_id
-        motion_update_msg.header.stamp = self.get_clock().now().to_msg()
-
-        motion_update_msg.target_stiffness = np.diag(
-            [90.0, 90.0, 90.0, 50.0, 50.0, 50.0]
-        ).flatten()
-        motion_update_msg.target_damping = np.diag(
-            [50.0, 50.0, 50.0, 20.0, 20.0, 20.0]
-        ).flatten()
-
-        motion_update_msg.feedforward_wrench_at_tip = Wrench(
-            force=Vector3(x=0.0, y=0.0, z=0.0), torque=Vector3(x=0.0, y=0.0, z=0.0)
-        )
-
-        motion_update_msg.wrench_feedback_gains_at_tip = [0.5, 0.5, 0.5, 0.0, 0.0, 0.0]
-
-        motion_update_msg.trajectory_generation_mode.mode = (
-            TrajectoryGenerationMode.MODE_POSITION
-        )
-
-        self.motion_update_pub.publish(motion_update_msg)
 
     def send_feedback(self, goal_handle, feedback):
         feedback_msg = InsertCable.Feedback()
